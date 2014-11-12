75d07a179443f7aca1d0ad3bd4661b3864a79377
jointasks <jointasks@sina.com>
2014-11-12 12:16:32 +0800

    init

---
+++ Krain/src/com/jointasks/krain/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jointasks.krain;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.activity_main);
+		
+	}
+
+}
