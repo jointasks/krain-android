0615d215c9001edcc383be8c6ddecb1e45da4353
jointasks <jointasks@sina.com>
2014-11-12 15:32:02 +0800

    init

--- Krain/src/com/jointasks/krain/MainActivity.java
+++ Krain/src/com/jointasks/krain/MainActivity.java
@@ -1,7 +1,10 @@
 package com.jointasks.krain;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class MainActivity extends Activity {
 
@@ -9,7 +12,14 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.activity_main);
-		
+		ActionBar actionBar = getActionBar();
+		actionBar.setDisplayHomeAsUpEnabled(true);
 	}
 
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.main_activity_actions, menu);
+		return super.onCreateOptionsMenu(menu);
+	}
 }
